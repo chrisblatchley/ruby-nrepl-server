ba3b5e30b85dbe70af281d3c72cb1f181233ca47
Chris Blatchley <chris@chrisblatchley.com>
2022-05-06 23:47:02 -0700

    refactor: server cleanup

--- lib/nrepl/server.rb
+++ lib/nrepl/server.rb
@@ -4,28 +4,40 @@ module NRepl
   class Server
     class << self
       def start(host: "0.0.0.0", port: "0", quiet: false)
-        server = TCPServer.new(host, port)
-        port = server.addr[1]
-        greet = "nREPL server started on port #{port} on host #{host} - nrepl://#{host}:#{port}"
-        puts greet
+        TCPServer.new(host, port).then do |server|
+          port = server.addr[1]
+          puts greet(host, port)
+          
+          File.write("nrepl.port", "#{port}\n")
 
-        {
-          server: server,
-          greet: greet
-        }
-      end
-
-      def listen_and_serve(server:, greet:)
-        conn = server.accept
-        session = NRepl::Session.start
-        conn.puts greet
-        while data = conn.gets
-          conn.puts Transport.handle(session, data)
+          {
+            server: server,
+            config: {
+              host: host,
+              port: port
+            }
+          }
         end
       end
 
-      def stop(server:)
+      def listen_and_serve(server:, config:)
+        server.accept.then do |conn|
+          NRepl::Session.start.then do |session|
+            conn.puts greet(config[:host], config[:port])
+            while data = conn.gets
+              conn.puts Transport.handle(session, data)
+            end
+          end
+        end
+      end
+
+      def stop(server:, **_)
+        File.delete("nrepl.port")
         server.close
+      end
+
+      def greet(host, port)
+        "nREPL server started on port #{port} on host #{host} - nrepl://#{host}:#{port}"
       end
     end
   end
