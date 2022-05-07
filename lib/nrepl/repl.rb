661a2ce462af6add027af54615d1e9d593945fd1
Chris Blatchley <chris@chrisblatchley.com>
2022-05-06 23:05:17 -0700

    feat: capture stdout

--- lib/nrepl/repl.rb
+++ lib/nrepl/repl.rb
@@ -4,13 +4,11 @@ module NRepl
   class Repl
     class << self
       def run(session, code)
-        {
+        wrap_stdout(code).merge({
           id: session[:id],
           ns: 'main',
-          out: '',
           err: nil,
-          value: eval(read(code))
-        }
+        })
       rescue Exception => e
         {
           id: session[:id],
@@ -19,6 +17,17 @@ module NRepl
           err: e.to_s,
           value: nil
         }
+      end
+
+      def wrap_stdout(code)
+        stdout, $stdout = $stdout, StringIO.new
+        value = eval(read(code))
+        {
+          out: $stdout.string.dump,
+          value: value
+        }
+      ensure
+        $stdout = stdout
       end
 
       def read(str)
