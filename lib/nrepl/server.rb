aed2d427efeea2f11077dbc65fd43211f2e22600
Chris Blatchley <chris@chrisblatchley.com>
2022-05-06 23:26:55 -0700

    feat: greet on new connection

--- lib/nrepl/server.rb
+++ lib/nrepl/server.rb
@@ -6,14 +6,19 @@ module NRepl
       def start(host: "0.0.0.0", port: "0", quiet: false)
         server = TCPServer.new(host, port)
         port = server.addr[1]
-        puts "nREPL server started on port #{port} on host #{host} - nrepl://#{host}:#{port}"
+        greet = "nREPL server started on port #{port} on host #{host} - nrepl://#{host}:#{port}"
+        puts greet
 
-        {server: server}
+        {
+          server: server,
+          greet: greet
+        }
       end
 
-      def listen_and_serve(server:)
+      def listen_and_serve(server:, greet:)
         conn = server.accept
         session = NRepl::Session.start
+        conn.puts greet
         while data = conn.gets
           conn.puts Transport.handle(session, data)
         end
